513cf6dd1946ffc3261c8550633d2dab9ed70ad8
MaineK00n <mainek00n.1229@gmail.com>
2021-11-16 07:32:27 +0900

    fix(fetcher): remove zero width space in CVEID

--- fetcher/kevuln.go
+++ fetcher/kevuln.go
@@ -10,16 +10,27 @@ import (
 	"github.com/MaineK00n/go-kev/utils"
 )
 
-var UTF8_BOM = []byte{239, 187, 191}
+var utf8Bom = []byte{239, 187, 191}
 
 func hasBOM(in []byte) bool {
-	return bytes.HasPrefix(in, UTF8_BOM)
+	return bytes.HasPrefix(in, utf8Bom)
 }
 
 func stripBOM(in []byte) []byte {
-	return bytes.TrimPrefix(in, UTF8_BOM)
+	return bytes.TrimPrefix(in, utf8Bom)
 }
 
+var zeroWidthSpace = []byte{226, 128, 139}
+
+func hasZeroWidthSpace(in []byte) bool {
+	return bytes.HasPrefix(in, zeroWidthSpace) || bytes.HasSuffix(in, zeroWidthSpace)
+}
+
+func stripZeroWidthSpace(in []byte) []byte {
+	return bytes.ReplaceAll(in, zeroWidthSpace, []byte{})
+}
+
+// FetchKEVuln :
 func FetchKEVuln() ([]models.KEVuln, error) {
 	url := "https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv"
 	log15.Info("Fetching", "URL", url)
@@ -36,5 +47,12 @@ func FetchKEVuln() ([]models.KEVuln, error) {
 	if err := gocsv.UnmarshalBytes(vulnCsv, &vulns); err != nil {
 		return nil, err
 	}
+
+	for i := range vulns {
+		if hasZeroWidthSpace([]byte(vulns[i].CveID)) {
+			vulns[i].CveID = string(stripZeroWidthSpace([]byte(vulns[i].CveID)))
+		}
+	}
+
 	return vulns, nil
 }
